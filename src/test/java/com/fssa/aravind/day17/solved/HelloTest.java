f8d2ef98698921870410a5db735e0ace31895378
ARAVIND RAM <116251979+aravindramvjs@users.noreply.github.com>
2023-06-23 10:32:16 +0530

    First Hello world program and pom.xml added

---
+++ src/test/java/com/fssa/aravind/day17/solved/HelloTest.java
@@ -0,0 +1,16 @@
+package com.fssa.aravind.day17.solved;
+
+import org.junit.jupiter.api.Assertions;
+
+import org.junit.jupiter.api.Test;
+
+public class HelloTest {
+
+	@Test //First test case
+	
+	public void helloTest() {
+
+		Assertions.assertEquals("Hello World", "Hello World");
+	}
+
+}
